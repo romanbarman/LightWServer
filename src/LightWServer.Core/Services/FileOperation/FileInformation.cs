2ba01e6dac0565b1dd878bae9ffef5dae0cd292a
Raman Zanko <r.zanko@godeltech.com>
2022-12-30 14:35:21 +0200

    Made members internal for FileInformation

--- src/LightWServer.Core/Services/FileOperation/FileInformation.cs
+++ src/LightWServer.Core/Services/FileOperation/FileInformation.cs
@@ -2,10 +2,10 @@
 {
     internal sealed class FileInformation
     {
-        public long Length { get; }
-        public string Extension { get; }
+        internal long Length { get; }
+        internal string Extension { get; }
 
-        public FileInformation(long length, string extension)
+        internal FileInformation(long length, string extension)
         {
             Length = length;
             Extension = extension;
